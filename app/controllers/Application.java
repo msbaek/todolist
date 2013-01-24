791c18e33a814f3059d3cac91cabdf33d748f90c
Myeongseok Baek <msbaek@daumcorp.com>
2013-01-24 20:55:47 +0900

    add action methods
    결과로 TODO를 반환
    아직 action method의 구현을 작성하려는 것이 아니므로
    빌트인으로 제공되는 TODO를 반환
    TODO는 "501 Not Implemented response"에 해당

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,9 +4,19 @@ import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Application extends Controller {
-  
-  public static Result index() {
-    return ok("Hello World");
-  }
-  
+    public static Result index() {
+        return ok("Hello World");
+    }
+
+    public static Result tasks() {
+        return TODO;
+    }
+
+    public static Result newTask() {
+        return TODO;
+    }
+
+    public static Result deleteTask(Long id) {
+        return TODO;
+    }
 }
