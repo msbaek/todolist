d79105b6a8fbe172a3cd36643bf585a4ca6c1c0e
Myeongseok Baek <msbaek@daumcorp.com>
2013-01-24 21:33:35 +0900

    form을 Application에 추가

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,13 @@
 package controllers;
 
+import models.Task;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Application extends Controller {
+    static Form<Task> taskForm = form(Task.class);
+
     public static Result index() {
         return redirect(routes.Application.tasks());
     }
